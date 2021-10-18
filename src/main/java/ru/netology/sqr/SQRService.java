ba7cfc4e78f0d76d7bd6f81399d5261250d8654e
Vladimir Kovalev <boban_envorter@mail.ru>
2021-10-18 19:30:15 +0300

    fixed

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,14 +2,16 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-    public int selection(int first, int second) {
+    public int pickNumbers(int first, int second) {
 
         int counter = 0;
 
         for (int i = 10; i <= 99; i++) {
 
+            int j = i * i;
 
-            if ((first <= (i * i) && (i * i) <= second)) {
+
+            if ((first <= j && j <= second)) {
 
                 counter = counter + 1;
 
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -7,14 +7,43 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @org.junit.jupiter.api.Test
-    void shouldSelection() {
+    void shouldPickNumbers() {
         SQRService service = new SQRService();
 
         int first = 100;
         int second = 200;
         int expected = 5;
 
-        int counter = service.selection(first, second);
+        int counter = service.pickNumbers(first, second);
+
+        assertEquals(expected, counter);
+
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void ShouldPickNumbersUnderBoard() {
+
+        SQRService service = new SQRService();
+
+        int first = 100;
+        int second = 101;
+        int expected = 1;
+        int counter = service.pickNumbers(first, second);
+
+        assertEquals(expected, counter);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void ShouldPickNumbersOverBoard() {
+
+        SQRService service = new SQRService();
+
+        int first = 100;
+        int second = 90;
+        int expected = 0;
+        int counter = service.pickNumbers(first, second);
 
         assertEquals(expected, counter);
 
